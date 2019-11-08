842f75b4aec00cc1ba9bdee8308c3ea8d9f80d5d
Majedul Hoque <rumanbsl@gmail.com>
2019-11-08 14:42:18 +0200

    Deref Example

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use tide::{App, Context};
+/* use tide::{App, Context};
 fn main() {
     let mut app = App::new();
 
@@ -29,4 +29,46 @@ impl Human for Person {
 enum List {
     Cons(i32, Box<List>),
     Nil
+}
+ */
+
+use std::ops::Deref;
+
+#[derive(Debug)]
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(item: T) -> MyBox<T> {
+        MyBox(item)
+    }
+}
+
+type Target<T> = T;
+impl<T> Deref for MyBox<T> {
+    type Target = Target<T>;
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
+
+fn main() {
+    let c = MyBox::new("Hello");
+    println!("{:?}", *c);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+    #[test]
+    fn deref_test() {
+        let a = 10;
+        let b = MyBox::new(a * 10);
+
+        println!("{:?}", *b);
+        assert_eq!(10, a);
+        // magick behind the scene
+        // b.deref() = &self.0 -> *(&self.0) -> 100
+        assert_eq!(100, *b);
+    }
 }
