32c984a6674c53d998c87b0e9f2113f6a21d4f62
Majedul Hoque <rumanbsl@gmail.com>
2019-11-12 20:11:44 +0200

    recursion example

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,43 @@
-trait Messenger {
-    fn send(&self, msg: &str);
+use std::cell::RefCell;
+use std::rc::Rc;
+
+type ConsList = RefCell<Rc<List>>;
+
+#[derive(Debug)]
+enum List {
+    Cons(i32, ConsList),
+    Undefined,
 }
-struct LimitTracker<'lifetime, T: Messenger> {
-    messenger: &'lifetime T,
-    value: usize,
-    max: usize,
-}
 
-impl<'lifetime, T: Messenger> LimitTracker<'lifetime, T> {
-    fn new(messenger: &T, max: usize) -> LimitTracker<T> {
-        LimitTracker {
-            messenger,
-            value: 0,
-            max,
-        }
-    }
+use List::{Cons, Undefined};
 
-    fn set_value(&mut self, value: usize) {
-        self.value = value;
-
-        let max_percentage = self.value as f64 / self.max as f64;
-
-        if max_percentage >= 1.0 {
-            self.messenger.send("Error, quota exceeded");
-        } else if max_percentage >= 0.9 {
-            self.messenger.send("90% limit reached");
-        } else if max_percentage >= 0.75 {
-            self.messenger.send("75% limit reached");
+impl List {
+    fn tail(&self) -> Option<&ConsList> {
+        match self {
+            Cons(_, val) => Some(val),
+            Undefined => None,
         }
     }
 }
 
-fn main() {}
+fn main() {
+    let a = Rc::new(Cons(5, RefCell::new(Rc::new(Undefined))));
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use pretty_assertions::assert_eq;
-    use std::cell::RefCell;
-    struct MockMessenger {
-        sent_messages: RefCell<Vec<String>>,
+    println!("a initial rc count = {}", Rc::strong_count(&a));
+    println!("a next item = {:?}", a.tail());
+
+    let b = Rc::new(Cons(10, RefCell::new(Rc::clone(&a))));
+
+    println!("a rc count after b creation = {}", Rc::strong_count(&a));
+    println!("b initial rc count = {}", Rc::strong_count(&b));
+    println!("b next item = {:?}", b.tail());
+
+    if let Some(link) = a.tail() {
+        *link.borrow_mut() = Rc::clone(&b);
     }
 
-    impl MockMessenger {
-        fn new() -> MockMessenger {
-            MockMessenger {
-                sent_messages: RefCell::new(vec![]),
-            }
-        }
-    }
-    impl Messenger for MockMessenger {
-        fn send(&self, value: &str) {
-            &self.sent_messages.borrow_mut().push(value.into());
-        }
-    }
-    #[test]
-    fn sends_over_90_percent_warning() {
-        let mock_messenger = MockMessenger::new();
-        let mut limit_tracker = LimitTracker::new(&mock_messenger, 100);
-        limit_tracker.set_value(90);
+    println!("b rc count after changing a = {}", Rc::strong_count(&b));
+    println!("a rc count after changing a = {}", Rc::strong_count(&a));
 
-        assert_eq!(mock_messenger.sent_messages.borrow().len(), 1);
-    }
+    // println!("a next item = {:?}", a.tail());
 }
